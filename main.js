624d233c4f2834e27973448891c50d4bb836f2a2
Rafaela Gomes <rafagiza@hotmail.com>
2024-05-10 21:22:31 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,16 @@
+console.log("Digite o nome do seu Herói")
+
+let nomeHeroi = "Sapolitana"
+
+let ferro  = 0 >= 1000
+let bronze = 1001 >= 2000
+let prata = 2001 >= 5000
+let ouro = 5001 >= 7000
+let platina = 7001 >= 8000
+let ascendente = 8001 >= 9000
+let imortal = 10001 >
+
+
+console.log("Bem vindo " + nomeHeroi)
+console.log(nomeHeroi + (" entrou no servidor"))
+console.log("O Herói " + nomeHeroi + " está no nível bronze com " + 1050 + " de xp")
