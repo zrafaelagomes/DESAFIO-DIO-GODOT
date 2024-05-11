ae887cc7cf40c1d0bc77d478940012c2210f75b8
Rafaela Gomes <rafagiza@hotmail.com>
2024-05-11 11:22:02 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,16 +1,25 @@
-console.log("Digite o nome do seu Herói")
+let nomeHeroi = prompt("Digite o nome do seu herói ")
+const xp = parseInt(prompt("Digite a quantidade de XP do herói:"));
 
-let nomeHeroi = "Sapolitana"
+let nivel;
 
-let ferro  = 0 >= 1000
-let bronze = 1001 >= 2000
-let prata = 2001 >= 5000
-let ouro = 5001 >= 7000
-let platina = 7001 >= 8000
-let ascendente = 8001 >= 9000
-let imortal = 10001 >
+if (xp < 1000) {
+  nivel = "Ferro";
+} else if (xp >= 1000 && xp < 2000) {
+  nivel = "Bronze";
+} else if (xp >= 2000 && xp < 5000) {
+  nivel = "Prata";
+} else if (xp >= 5000 && xp < 7000) {
+  nivel = "Ouro";
+} else if (xp >= 7000 && xp < 8000) {
+  nivel = "Platina";
+} else if (xp >= 8000 && xp < 9000) {
+  nivel = "Ascendente";
+} else if (xp >= 9000 && xp < 10000) {
+  nivel = "Imortal";
+} else {
+  nivel = "Radiante";
+}
 
-
-console.log("Bem vindo " + nomeHeroi)
-console.log(nomeHeroi + (" entrou no servidor"))
-console.log("O Herói " + nomeHeroi + " está no nível bronze com " + 1050 + " de xp")
+console.log("Digite o nome do seu herói " + nomeHeroi)
+console.log("O herói de nome " + nomeHeroi + " está no nível ", nivel)
